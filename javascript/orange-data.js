d4597da6d6f998b6e6b8ca5115460901bcd9662c
Stephen Bannasch <stephen.bannasch@gmail.com>
2015-04-22 14:03:10 -0400

    remove orange high school location from map

--- javascript/orange-data.js
+++ javascript/orange-data.js
@@ -52,19 +52,11 @@ locationAndContentData = {
       ]
     },
     {
-      "address": "400 Lincoln Ave",
-      "latitude": 40.765656,
-      "longitude": -74.239035,
-      "contentItems": [
-        1
-      ]
-    },
-    {
       "address": "475 South Jefferson Street",
       "latitude": 40.764822,
       "longitude": -74.246701,
       "contentItems": [
-        2
+        1
       ]
     },
     {
@@ -72,7 +64,7 @@ locationAndContentData = {
       "latitude": 40.767493,
       "longitude": -74.226182,
       "contentItems": [
-        3
+        2
       ]
     }
   ],
@@ -85,21 +77,15 @@ locationAndContentData = {
     },
     {
       "index": 1,
-      "title": "Orange High School Vs Barringer High School",
-      "video": "media/video/480x270/Orange_High_School_Vs_Barringer_High_School",
+      "title": "Hidden Treasures of Orange, Hat City and No Name Factory",
+      "video": "media/video/480x270/Hidden_Treasures_of_Our_Orange_Hat_City_No_Name",
       "location": 1
     },
     {
       "index": 2,
-      "title": "Hidden Treasures of Orange, Hat City and No Name Factory",
-      "video": "media/video/480x270/Hidden_Treasures_of_Our_Orange_Hat_City_No_Name",
-      "location": 2
-    },
-    {
-      "index": 3,
       "title": "Highway 280",
       "video": "media/video/480x270/Hidden_Treasures_of_Our_Orange_Highway_280_with",
-      "location": 3
+      "location": 2
     }
   ]
 };
